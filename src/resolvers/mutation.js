5a3185a8bd537e0249a45f7c1cf39bbfb0c42fdf
Peggy Rayzis <peggyrayzis@gmail.com>
2018-06-26 00:06:55 -0500

    Clarified mutation instructions

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -5,6 +5,10 @@ fieldName: (obj, args, context, info) => result;
 
 Check models/movie for the data fetching functions you'll need to complete the exercise.
 Refer to your schema if you're unsure what to return from the resolvers.
+
+For toggleLike, only authenticated users can toggle a like. Try grabbing the user
+off the context and throw an error if there is no user.
+
 Good luck!
 */
 
