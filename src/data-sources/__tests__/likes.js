e96966823bd0627888c766aa492089345aa0bd49
Jake <dawkinsjh@gmail.com>
2018-09-27 09:43:22 -0400

    updated likes tests

--- src/data-sources/__tests__/likes.js
+++ src/data-sources/__tests__/likes.js
@@ -1,36 +1,62 @@
-it('', () => {});
+/**
+ * The purpose of dataSource tests:
+ * 1. validate/transform any args
+ *    - see ./movie ([getMovies] sets sort param properly)
+ * 2. make sure data returned from APIs is transformed properly
+ *    - see ./movie ([getMovies] returns empty array if no api response)
+ * 3. calls any other underlying models with correct args
+ *    - see ./movie ([getMovieLikes] calls store method and returns results)
+ */
 
-// it('[getMovieLikes] calls store method and returns results', async () => {
-//   const mockLikes = [{ id: 1, user: 1 }];
-//   moviesAPI.store.likes.findAll.mockReturnValueOnce(mockLikes);
-//   const res = await moviesAPI.getMovieLikes({ user: 1 });
+const LikesAPI = require('../likes');
 
-//   expect(moviesAPI.store.likes.findAll).toBeCalledWith({
-//     where: { user: 1 },
-//   });
-//   expect(res).toEqual(mockLikes);
-// });
+describe('LikesAPI', () => {
+  let likesAPI;
 
-// it('[toggleMovieLikes] creates or destroys likes, based on current', async () => {
-//   // mock the return of the find ONCE, and expect destroy to be called to remove it
-//   const mockLikes = [{ id: 1, user: 1 }];
-//   moviesAPI.store.likes.find.mockReturnValueOnce(mockLikes);
-//   await moviesAPI.toggleMovieLike({ user: 1, movie: 1 });
-//   expect(moviesAPI.store.likes.destroy).toBeCalled();
+  beforeEach(() => {
+    likesAPI = new LikesAPI();
+    likesAPI.store = {
+      likes: {
+        findAll: jest.fn(),
+        find: jest.fn(),
+        create: jest.fn(),
+        destroy: jest.fn(),
+      },
+    };
+  });
 
-//   // no likes found -- should create one
-//   await moviesAPI.toggleMovieLike({ user: 1, movie: 1 });
-//   expect(moviesAPI.store.likes.create).toBeCalled();
-// });
+  it('[getMovieLikes] calls store method and returns results', async () => {
+    const mockLikes = [{ id: 1, user: 1 }];
+    likesAPI.store.likes.findAll.mockReturnValueOnce(mockLikes);
+    const res = await likesAPI.getMovieLikes({ user: 1 });
 
-// it('[isMovieLiked] finds like if present', async () => {
-//   // mock the return of the find ONCE. expect return to be true -- is liked
-//   const mockLikes = [{ id: 1, user: 1 }];
-//   moviesAPI.store.likes.find.mockReturnValueOnce(mockLikes);
-//   const resTrue = await moviesAPI.isMovieLiked({ user: 1, movie: 1 });
-//   expect(resTrue).toBeTruthy();
+    expect(likesAPI.store.likes.findAll).toBeCalledWith({
+      where: { user: 1 },
+    });
+    expect(res).toEqual(mockLikes);
+  });
 
-//   // no likes found -- should be false
-//   const resFalse = await moviesAPI.isMovieLiked({ user: 1, movie: 1 });
-//   expect(resFalse).toBeFalsy();
-// });
+  it('[toggleMovieLikes] creates or destroys likes, based on current', async () => {
+    // mock the return of the find ONCE, and expect destroy to be called to remove it
+    const mockLikes = [{ id: 1, user: 1 }];
+    likesAPI.store.likes.find.mockReturnValueOnce(mockLikes);
+    await likesAPI.toggleMovieLike({ user: 1, movie: 1 });
+    expect(likesAPI.store.likes.destroy).toBeCalled();
+
+    // no likes found -- should create one
+    await likesAPI.toggleMovieLike({ user: 1, movie: 1 });
+    expect(likesAPI.store.likes.create).toBeCalled();
+  });
+
+  it('[isMovieLiked] finds like if present', async () => {
+    // mock the return of the find ONCE. expect return to be true -- is liked
+    const mockLikes = [{ id: 1, user: 1 }];
+    likesAPI.store.likes.find.mockReturnValueOnce(mockLikes);
+    const resTrue = await likesAPI.isMovieLiked({ user: 1, movie: 1 });
+    expect(resTrue).toBeTruthy();
+
+    // no likes found -- should be false
+    const resFalse = await likesAPI.isMovieLiked({ user: 1, movie: 1 });
+    expect(resFalse).toBeFalsy();
+  });
+});
